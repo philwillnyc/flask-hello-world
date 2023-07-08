28c9894eb3eed8ee4756a2859de5d6da6494a51d
philwillnyc <56197213+philwillnyc@users.noreply.github.com>
2023-07-08 19:24:00 -0400

    Added the main files

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, session
+from flask_session import Session
+
+from model import predict
+
+#Create the flask app.
+
+app = Flask(__name__)
+
+#Set up individual sessions so multiple users don't conflict. 
+
+app.secret_key = '123456789'
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_PERMANENT'] = False
+server_session = (Session(app))
+
+#Computational functions.
+
+def compute_bmi(height,weight):
+    """Height in inches, weight in lbs."""
+    return 703*(weight/(height**2))
+
+def check_prediabetes(a1c,glucose):
+    return int(glucose > 100 or a1c > 5.6)
+
+#Homepage.
+
+@app.route('/', methods=['GET'])
+def home():
+    session['prediction'] = 'Enter information above to predict risk of gestational diabetes.'
+    return render_template(
+                'predict.html', 
+                prediction = session['prediction'],
+                    )
+
+#Calculate risk. 
+
+@app.route('/', methods=['POST'])
+def compute():
+    r = request.form
+    try:
+        dia_bp = int(r['diastolic'])
+        sys_bp = int(r['systolic'])
+        hdl = int(r['hdl'])
+        bmi = compute_bmi(int(r['height']),int(r['weight']))
+        age = int(r['age'])
+        prediabetes = check_prediabetes(float(r['a1c']),int(r['glucose']))
+        no_prediabetes = "" if prediabetes else "no"
+        percent = f'{(int(100*round(predict(dia_bp,sys_bp,hdl,bmi,age,prediabetes),2)))}%'
+        session['prediction'] = f"""
+        A {age} year old female with a BMI of {round(bmi,1)}, a serum HDL cholesterol of {hdl}, blood pressure of {sys_bp} 
+        over {dia_bp}, and {no_prediabetes} prediabetes has a {percent} chance of developing gestational diabetes in her next pregnancy. 
+        """
+    except:
+        session['prediction'] = 'Error, please check your inputs and try again.'
+    return render_template(
+                'predict.html', 
+                prediction = session['prediction'],
+                    )
+
+app.run(debug = True)
---
+++ model.py
@@ -0,0 +1,59 @@
+"""Generates a Logistic Regression model for the gestational diabetes data."""
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+
+from joblib import dump, load
+
+df = pd.read_csv('gestationalDiabetes.csv', index_col = 0)
+
+#Deal with nan.
+
+#HDL and BMI.
+HDL_mean = np.mean(df['HDL'])
+BMI_mean = np.mean(df['BMI'])
+df['HDL'].fillna(HDL_mean, inplace = True)
+df['BMI'].fillna(BMI_mean, inplace = True)
+
+#Sys BP.
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+bp = df[['Sys BP','Dia BP']].dropna()
+reg.fit(bp[['Dia BP']], bp[['Sys BP']])
+predicted_bp = reg.predict(df[['Dia BP']])
+predicted_bp = [x[0] for x in predicted_bp] #need to flatten so that fillna can handle it
+df['Sys BP'] = df['Sys BP'].fillna(pd.Series(predicted_bp))
+
+#All measurements typically done during an annual physical.
+df_physical = df[['Dia BP',
+                            'Sys BP',
+                            'HDL',
+                           'BMI', 
+                            'Age',
+                            'Prediabetes',
+                            'Class Label(GDM /Non GDM)']]
+
+#Train a Random Forests Model.
+X = df_physical[['Dia BP',
+                            'Sys BP',
+                            'HDL',
+                           'BMI', 
+                            'Age',
+                            'Prediabetes',]]
+y = df_physical[['Class Label(GDM /Non GDM)']]
+X_train, X_test, y_train, y_test  = train_test_split(X,y)
+model = RandomForestClassifier()
+model.fit(X_train,y_train)
+
+#Save the model.
+dump(model,'model.joblib')
+
+#Create the predictor.
+def predict(dia_bp, sys_bp, hdl, bmi, age, prediabetes):
+    """Input physical parameters and returns a probability of gestational diabetes."""
+    model = load('model.joblib')
+    _, result = model.predict_proba([[dia_bp,sys_bp,hdl,bmi,age,prediabetes]])[0]
+    return result
